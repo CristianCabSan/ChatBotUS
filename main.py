53a4989e9f0aa105234c4658d029bb47cc523be9
Cristian Caballero Sanchez <cricabsan2@us.es>
2023-11-28 15:29:13 +0100

    Cambio

---
+++ main.py
@@ -0,0 +1,4 @@
+import querys
+
+user_input = input("Enter the parameter: ")
+querys.buscar_url(user_input)
--- querys.py
+++ querys.py
@@ -18,9 +18,10 @@ def make_query(parameter):
     #Makes the initial query to get the ID of the related papers using the esearch function
     urlEsearch = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi"
     paramsEsearch = {
+        #Others params such as date, or authors goes here
         'db': 'pmc',
-        'term': parameter,
         'maxdate': '2021',
+        'term': parameter, # term=parameter
     }
 
     try:
@@ -30,26 +31,60 @@ def make_query(parameter):
         # Parse the XML response
         xmlEsearch = ET.fromstring(response.content)
 
-        # Find the IdList element
+        # Get the IdList field
         id_list_element = xmlEsearch.find(".//IdList")
 
         # Output the IdList element and its content
-        if id_list_element is not None:
-            id_list_content = ET.tostring(id_list_element, encoding='unicode')
-            IDs = listarIDs(id_list_content)
-            urlIDs = [f"https://www.ncbi.nlm.nih.gov/research/bionlp/RESTful/pmcoa.cgi/BioC_xml/PMC{pmc_id}/unicode"for pmc_id in IDs]
-            
-        else:
-            print("IdList not found in the XML response.")
+        id_list_content = ET.tostring(id_list_element, encoding='unicode')
+        IDs = listarIDs(id_list_content)
+        urlIDs = [f"https://www.ncbi.nlm.nih.gov/research/bionlp/RESTful/pmcoa.cgi/BioC_xml/PMC{pmc_id}/unicode"for pmc_id in IDs]
+        for url in urlIDs:
+            file = requests.get(url)
+            file.raise_for_status()
+
+            xmlID = ET.fromstring(file.content)
+            xmlID2 = xmlID.find("./document/passage")
+            xmlID3 = ET.tostring(xmlID2, encoding='unicode')
+            print("-----------------PAPER:", url, "----------------\n", xmlID3)
+
 
     except requests.exceptions.RequestException as e:
         print(f"Error making request: {e}")
     except ET.ParseError as e:
         print(f"Error parsing XML: {e}")
-# Example usage:
-user_input = input("Enter the parameter: ")
-make_query(user_input)
 
 
 
+def buscar_url(parameter):
+    #Makes the initial query to get the ID of the related papers using the esearch function
+    urlEsearch = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi"
+    paramsEsearch = {
+        #Others params such as date, or authors goes here
+        'db': 'pmc',
+        'retmax' : 5 ,
+        'term': parameter, # term=parameter
+        'field': 'abstract',
+        'field': 'title'
+    }
 
+    try:
+        response = requests.get(urlEsearch, params=paramsEsearch)
+        response.raise_for_status() #Error control
+
+        # Parse the XML response
+        xmlEsearch = ET.fromstring(response.content)
+
+        # Get the IdList field
+        id_list_element = xmlEsearch.find(".//IdList")
+
+        # Output the IdList element and its content
+        id_list_content = ET.tostring(id_list_element, encoding='unicode')
+        IDs = listarIDs(id_list_content)
+        urlIDs = [f"https://www.ncbi.nlm.nih.gov/research/bionlp/RESTful/pmcoa.cgi/BioC_xml/PMC{pmc_id}/unicode"for pmc_id in IDs]
+        i = 0
+        for url in urlIDs:
+            print(i,": ", url)
+            i += 1
+    
+    except requests.exceptions.RequestException as e:
+        print(f"Error making request: {e}")
